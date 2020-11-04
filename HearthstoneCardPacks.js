222848904fbfe0c4cd4bc5e94c714578c4dd602b
scottdavidschroederjr <72458330+scottdavidschroederjr@users.noreply.github.com>
2020-11-03 21:36:41 -0500

    Add files via upload

---
+++ HearthstoneCardPacks.js
@@ -0,0 +1,314 @@
+// Global Variables //
+let sims = 1000
+let resultsArray = [];
+let set = "";
+let packsToBuy = 0;
+
+let dust = 0;
+let packCount = 0;
+let roll = 0;
+let cost = 0;
+
+let setCommon = 0;
+let setRare = 0;
+let setEpic = 0;
+let setLegendary = 0;
+
+let ownedCommons = 0;
+let ownedRares = 0;
+let ownedEpics = 0;
+let ownedLegendaries = 0;
+let ownedDust = 0;
+
+let commonCount = 0;
+let rareCount = 0;
+let epicCount = 0;
+let legendaryCount = 0;
+
+let commonArray = [];
+let rareArray = [];
+let epicArray = [];
+let legendaryArray = [];
+let cardPack = [];
+
+let epicPityCounter = 0;
+let legendaryPityCounter = 30;
+
+
+// How Long Until Set Completion? //
+function completionSim(selectedSet,ownedCommons = 0,ownedRares = 0, ownedEpics = 0, ownedLegendaries = 0) {
+    for (let tests = 0; tests < sims; tests++) {
+
+        packCount = 0;
+        set = selectedSet
+
+        commonArray = [];
+        rareArray = [];
+        epicArray = [];
+        legendaryArray = [];
+
+        epicPityCounter = 0;
+        legendaryPityCounter = 30;
+
+// Cards Already Owned //
+        function alreadyOwned() {
+        if (ownedCommons > 0) {
+            for (i = 0; i < ownedCommons; i++) {
+                commonArray.push(i);
+            }
+            commonCount = ownedCommons;
+        }
+        if (ownedRares > 0) {
+            for (let i = 0; i < ownedRares; i++) {
+                rareArray.push(i);
+            }
+            rareCount = ownedRares
+        }
+        if (ownedEpics > 0) {
+            for (let i = 0; i < ownedEpics; i++) {
+                epicArray.push(i);
+            }
+            epicCount = ownedEpics;
+        }
+        if (ownedLegendaries > 0) {
+            for (i = 0; i < ownedLegendaries; i++) {
+                legendaryArray.push(i);
+            }
+            legendaryCount = ownedLegendaries;
+            legendaryPityCounter = 0;
+        }
+    }
+
+// Set Selection //
+        function setSelection() {
+            if (set == "Classic Set") {
+                setCommon = 52;
+                setRare = 35;
+                setEpic = 23;
+                setLegendary = 25;
+            } else if (set == "Goblins vs Gnomes") {
+                setCommon = 40;
+                setRare = 37;
+                setEpic = 26;
+                setLegendary = 20;
+            } else if (set == "The Grand Tournament") {
+                setCommon = 49;
+                setRare = 36;
+                setEpic = 27;
+                setLegendary = 20;
+            } else if (set == "Whispers of the Old Gods") {
+                setCommon = 50;
+                setRare = 36;
+                setEpic = 27;
+                setLegendary = 21;
+            } else if (set == "Mean Streets of Gadgetzan") {
+                setCommon = 49;
+                setRare = 36;
+                setEpic = 27;
+                setLegendary = 20;
+            } else if (set == "Journey to UnGoro" || set == "Knights of the Frozen Throne" || set == "Kobolds & Catacombs" || set == "Rastakhan Rumble" || set == "Uldum") {
+                setCommon = 49;
+                setRare = 36;
+                setEpic = 27;
+                setLegendary = 23;
+            } else if (set == "The Witchwood") {
+                setCommon = 48;
+                setRare = 35;
+                setEpic = 25;
+                setLegendary = 21;
+            } else if (set == "The Boomsday Project") {
+                setCommon = 48;
+                setRare = 35;
+                setEpic = 25;
+                setLegendary = 21;
+            } else if (set == "Rise of Shadows") {
+                setCommon = 49;
+                setRare = 37;
+                setEpic = 26;
+                setLegendary = 24;
+            } else if (set == "Descent of Dragons") {
+                setCommon = 49;
+                setRare = 36;
+                setEpic = 27;
+                setLegendary = 28;
+            } else if (set == "Ashes of Outland" || set == "Scholomance Academy") {
+                setCommon = 49;
+                setRare = 36;
+                setEpic = 27;
+                setLegendary = 28;
+            }
+        }
+
+
+        setSelection();
+        alreadyOwned();
+        collectionCheck();
+    }
+
+}
+
+// Completion Check //
+function collectionCheck() {
+    if (commonArray.length === (setCommon * 2) &&
+        rareArray.length === (setRare * 2) &&
+        epicArray.length === (setEpic * 2) &&
+        legendaryArray.length === setLegendary)
+    {result()}
+    else if ((
+        (40 * ((setCommon * 2) - commonArray.length)) +
+        (100 * ((setRare * 2) - rareArray.length)) +
+        (400 * ((setEpic * 2) - epicArray.length)) +
+        (1600 * (setLegendary - legendaryArray.length)))
+        <= dust)
+    {result()}
+    else {
+        packCount++;
+        selectRarity();
+    }
+
+}
+
+// Rarity Selection //
+function selectRarity() {
+    cardPack = [];
+    for (i = 0; cardPack.length < 5; i++) {
+     roll = Math.random();
+     let legendaryProb = (0.01127+1.17350*(1/(41-legendaryPityCounter)));
+     let epicProb = (0.06305+1.03953*(1/(11-epicPityCounter)));
+     let rareProb = ((0.229*(1-(legendaryProb+epicProb)))/0.955);
+     let commonProb = (1-(legendaryProb+epicProb+rareProb));
+
+     if (roll <= legendaryProb) {
+         cardPack[i] = "Legendary";
+         legendaryPityCounter = 0;
+     }
+     else if (roll > legendaryProb && roll <= legendaryProb + epicProb) {
+         cardPack[i] = "Epic";
+         epicPityCounter = 0;
+     }
+     else if (roll > legendaryProb + epicProb && roll <= legendaryProb + epicProb + rareProb) {
+         cardPack[i] = "Rare";
+     }
+     else if (roll > legendaryProb + epicProb + rareProb && roll <= 1) {
+         cardPack[i] = "Common"
+     }
+    }
+    // Five Commons Protection & Pity Timer Update //
+
+    if (cardPack[0] === "Common" &&
+        cardPack[1] === "Common" &&
+        cardPack[2] === "Common" &&
+        cardPack[3] === "Common" &&
+        cardPack[4] === "Common") {
+        cardPack[4] === "Rare";
+    }
+
+    if (cardPack[0] != "Legendary" &&
+        cardPack[1] != "Legendary" &&
+        cardPack[2] != "Legendary" &&
+        cardPack[3] != "Legendary" &&
+        cardPack[4] != "Legendary") {
+        legendaryPityCounter++
+    }
+    if (cardPack[0] != "Epic" &&
+        cardPack[1] != "Epic" &&
+        cardPack[2] != "Epic" &&
+        cardPack[3] != "Epic" &&
+        cardPack[4] != "Epic") {
+        epicPityCounter++
+    }
+    selectCard();
+
+
+}
+
+// Card Selection //
+function selectCard() {
+    for (let i=0; i < 5; i++) {
+        if (cardPack[i] === "Common") {
+            if (commonArray.length === (setCommon * 2)) {
+                dust = dust + 5;
+            }
+            else {
+                commonArray.push(commonCount);
+                commonCount++;
+            }
+        }
+        else if (cardPack[i] === "Rare") {
+            if (rareArray.length === (setRare * 2)) {
+                dust = dust + 20;
+            }
+            else {
+                rareArray.push(rareCount);
+                rareCount++;
+            }
+        }
+        else if (cardPack[i] === "Epic") {
+            if (epicArray.length === (setEpic * 2)) {
+                dust = dust + 100;
+            }
+            else {
+                epicArray.push(epicCount);
+                epicCount++;
+            }
+        }
+        else if (cardPack[i] === "Legendary") {
+            if (legendaryArray.length === setLegendary) {
+                dust = dust + 400;
+            }
+            else {
+                legendaryArray.push(legendaryCount);
+                legendaryCount++;
+            }
+        }
+    }
+    collectionCheck();
+}
+
+// Results //
+function result() {
+    if (resultsArray.length < sims) {
+        resultsArray.push(packCount);
+
+        dust = 0;
+        packCount = 0;
+
+        commonCount = 0;
+        rareCount = 0;
+        epicCount = 0;
+        legendaryCount = 0;
+
+        commonArray = [];
+        rareArray = [];
+        epicArray = [];
+        legendaryArray = [];
+
+        ownedCommons = 0;
+        ownedRares = 0;
+        ownedEpics = 0;
+        ownedLegendaries = 0;
+
+        epicPityCounter = 0;
+        legendaryPityCounter = 30;
+    }
+    if (resultsArray.length == sims) {
+        let average = Math.round(((resultsArray.reduce(function(a,b){return a+b;}))/resultsArray.length));
+        cost = Math.round(average*1.250);
+
+
+
+        document.getElementById("resultsOutput").innerHTML=(set + " will take " + average + " packs to complete.");
+        document.getElementById("costOutput").innerHTML=("Estimated Cost of Completion: $" + cost);
+
+
+
+
+
+
+
+        cost = 0;
+        resultsArray = [];
+    }
+}
+
+
